539d00d4356fdf50aa0f0ca6e633db587df9982a
Daniel Varcas <daniel.varcas@gmail.com>
2018-12-11 15:17:23 +0000

    first test

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,9 @@
+describe('noughts and crosses', () => {
+  beforeEach(() => {
+    cy.visit('') // defaults to index.html
+  })
+
+  it('exists', () => {
+    cy.visit('')
+  });
+});
--- index.js
+++ index.js
@@ -3,29 +3,44 @@ const rows = document.querySelector("tr");
 const cells = [...document.getElementsByTagName('td')];
 const statusBar = document.querySelector("#statusBar");
 
-const game = { turn: 'O' };
+const game = {
+  turn: 'O',
+  board: {
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+    5: null,
+    6: null,
+    7: null,
+    8: null,
+    9: null
+  }
+};
 
-console.log(board);
-console.log(rows);
-console.log(cells);
+// console.log(board);
+// console.log(rows);
+// console.log(cells);
 
 cells.forEach(cell => {
   cell.addEventListener("click", addMark);
-
 });
 
 function addMark(event) {
   if (this.innerHTML === '') {
     this.innerHTML = game.turn;
+    updateBoard(game, event)
     takeTurn(game);
     updateStatus(statusBar);
+    console.log(game)
   }
 }
 
 function updateStatus(statusBar) {
+  const oldStatus = statusBar.childNodes[3];
   const newStatus = document.createElement('p');
   newStatus.innerHTML = game.turn;
-  statusBar.appendChild(newStatus)
+  statusBar.replaceChild(newStatus, oldStatus)
 }
 
 function takeTurn(game) {
@@ -36,3 +51,9 @@ function takeTurn(game) {
   }
 }
 
+function updateBoard(game, event) {
+  console.log(game)
+  console.log(event)
+  const cell = event.target.id;
+  game.board[cell] = game.turn;
+}

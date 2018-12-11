f82919347375d35dfd75e238a7bb6416e37075b8
john12321 <johnomeara72@gmail.com>
2018-12-11 14:02:50 +0000

    1pm handover

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const board = document.querySelector("#board")
-const rows = document.querySelector("tr")
-const cells = [...document.getElementsByTagName('td')]
+const board = document.querySelector("#board");
+const rows = document.querySelector("tr");
+const cells = [...document.getElementsByTagName('td')];
+const statusBar = document.querySelector("#statusBar");
 
-const game = { turn: 'O' }
+const game = { turn: 'O' };
 
-console.log(board)
-console.log(rows)
-console.log(cells)
+console.log(board);
+console.log(rows);
+console.log(cells);
 
 cells.forEach(cell => {
-  cell.addEventListener("click", addMark)
-})
+  cell.addEventListener("click", addMark);
+
+});
 
 function addMark(event) {
   if (this.innerHTML === '') {
     this.innerHTML = game.turn;
     takeTurn(game);
+    updateStatus(statusBar);
   }
+}
 
+function updateStatus(statusBar) {
+  const newStatus = document.createElement('p');
+  newStatus.innerHTML = game.turn;
+  statusBar.appendChild(newStatus)
 }
 
 function takeTurn(game) {
